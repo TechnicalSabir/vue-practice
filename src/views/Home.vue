20f454a28206ab25ebaff8b71a594b6bd6ae872a
TechnicalSabir <developer.sabir@gmail.com>
2025-01-27 22:38:01 +0530

    PR-11: Get input value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,24 @@ export default {
   name: "Home",
   data() {
     return {
-      count: 0,
+      email: "",
+      password: "",
     }
   },
+  methods: {
+    getData() {
+      console.table(this.$data)
+    }
+  }
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
     <h1 class="text-3xl font-bold" v-on:mouseenter="getConsole()">Home Page</h1>
-    <input type="text" placeholder="Enter value" class="border border-gray-500 rounded-md py-1 px-2" v-model="count" />
-    <h2>{{ count }}</h2>
+    <input type="text" placeholder="Enter email" class="border m-2 border-gray-500 rounded-md py-1 px-2"
+      v-model="email" />
+    <input type="text" placeholder="Enter password" class="border m-2 border-gray-500 rounded-md py-1 px-2"
+      v-model="password" />
+    <button type="button" class="bg-green-600 p-2 rounded-md cursor-pointer" @click="getData()">Get value</button>
   </main>
 </template>
