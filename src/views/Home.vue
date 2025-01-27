31ccc813ab2eb3092fc5afce8f45914669632fdb
TechnicalSabir <developer.sabir@gmail.com>
2025-01-27 22:25:09 +0530

    PR-10 Two way binding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,17 @@
 <script>
 export default {
   name: "Home",
-  data(){
+  data() {
     return {
       count: 0,
     }
   },
-  methods: {
-    getData(data){
-      alert(data);
-    },
-    getConsole(data){
-      console.log("Focusing an element.")
-    }
-  }
- 
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
     <h1 class="text-3xl font-bold" v-on:mouseenter="getConsole()">Home Page</h1>
-    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="getData(`Button1 clicked.`)">Single click1</button>
-    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="getData(`Button2 clicked.`)">Single click2</button>
-    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:dblclick="getData(`Button1 Double clicked.`)">Double click1</button>
-    <button type="button" class="bg-blue-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="count++">Count: {{ count }}</button>
-
+    <input type="text" placeholder="Enter value" class="border border-gray-500 rounded-md py-1 px-2" v-model="count" />
+    <h2>{{ count }}</h2>
   </main>
 </template>
