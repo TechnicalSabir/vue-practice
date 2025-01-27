ee82bd642e3da3675051e33acef31ed2ac92d7ed
TechnicalSabir <developer.sabir@gmail.com>
2025-01-27 22:18:06 +0530

    PR-9 Event with example, click event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,29 @@
 <script>
 export default {
   name: "Home",
-  data() {
+  data(){
     return {
-      email: "admin@gmail.com",
-      mobile: 9988776655,
+      count: 0,
     }
   },
   methods: {
-    getName(name) {
-      return name
+    getData(data){
+      alert(data);
     },
-    getData() {
-      return {
-        name: "Harry",
-        email: this.email
-      }
+    getConsole(data){
+      console.log("Focusing an element.")
     }
   }
+ 
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
-    <h1 class="text-3xl font-bold">Home Page</h1>
-    <h1>Email: {{ email }}</h1>
-    <h1>Name: {{ getName("John Wick") }}</h1>
-    <h1>Name2: {{ getName("Bruce Banner") }}</h1>
-    <h1>All Data: {{ getData() }}</h1>
-    <h1>Name: {{ getData().name }}</h1>
-    <h1>Email: {{ getData().email }}</h1>
+    <h1 class="text-3xl font-bold" v-on:mouseenter="getConsole()">Home Page</h1>
+    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="getData(`Button1 clicked.`)">Single click1</button>
+    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="getData(`Button2 clicked.`)">Single click2</button>
+    <button type="button" class="bg-red-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:dblclick="getData(`Button1 Double clicked.`)">Double click1</button>
+    <button type="button" class="bg-blue-500 text-white rounded-md px-2 py-1 text-sm cursor-pointer m-2" v-on:click="count++">Count: {{ count }}</button>
 
   </main>
 </template>
