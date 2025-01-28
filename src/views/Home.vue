6ead1cb9fcbb64c1597610c83bb83c62524ce421
TechnicalSabir <developer.sabir@gmail.com>
2025-01-28 22:06:29 +0530

    PR-13: If else conditions - conditional rendering

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,57 +3,18 @@ export default {
   name: "Home",
   data() {
     return {
-      // technology: ["PHP", "Java", "Javascript"],
-      technology: [],
-      user: "",
-    }
-  },
-  methods: {
-    getData() {
-      console.table(this.$data)
+      show: false,
     }
   }
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
-    <h1 class="text-3xl font-bold" v-on:mouseenter="getConsole()">Home Page</h1>
-    <input type="text" placeholder="Enter email" class="border m-2 border-gray-500 rounded-md py-1 px-2"
-      v-model="email" />
-    <input type="text" placeholder="Enter password" class="border m-2 border-gray-500 rounded-md py-1 px-2"
-      v-model="password" />
+    <h1 class="text-3xl font-bold">Home Page</h1>
+    <h1 v-if="show">If condition 1</h1>
+    <h1 v-if="show">If condition 2</h1>
+    <h1 v-else>else condition 1</h1>
+    <button @click="show=!show" class="bg-blue-400 p-2 rounded-md cursor-pointer">Toggle</button>
 
-    <div class="my-3">
-      <h1 class="font-bold">Technology</h1>
-      <div class="form-control">
-        <input type="checkbox" class="cursor-pointer mx-2" id="javaLabel" value="Java" v-model="technology">
-        <label for="javaLabel" class="cursor-pointer">Java</label>
-      </div>
-      <div class="form-control">
-        <input type="checkbox" class="cursor-pointer mx-2" id="phpLabel" value="PHP" v-model="technology">
-        <label for="phpLabel" class="cursor-pointer">PHP</label>
-      </div>
-      <div class="form-control">
-        <input type="checkbox" class="cursor-pointer mx-2" id="NodejsLabel" value="Javascript" v-model="technology">
-        <label for="NodejsLabel" class="cursor-pointer">Javascript</label>
-      </div>
-    </div>
-    <div class="my-2">
-      <span>Selected technologies: {{ technology }}</span>
-    </div>
-
-    <h2 class="font-bold">Who i am: {{ user }}</h2>
-    <div class="my-3">
-      <div class="form-control">
-        <input type="radio" class="cursor-pointer mx-2" name="user" id="studentLabel" value="Student" v-model="user">
-        <label for="studentLabel" class="cursor-pointer">Student</label>
-      </div>
-      <div class="form-control">
-        <input type="radio" class="cursor-pointer mx-2" name="user" id="developerLabel" value="Developer"
-          v-model="user">
-        <label for="developerLabel" class="cursor-pointer">Developer</label>
-      </div>
-    </div>
-    <button type="button" class="bg-green-600 p-2 rounded-md cursor-pointer" @click="getData()">Get value</button>
   </main>
 </template>
