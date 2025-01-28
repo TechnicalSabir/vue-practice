15abcb34f40d0f26df2af966c4db87e2fb383996
TechnicalSabir <developer.sabir@gmail.com>
2025-01-28 23:04:40 +0530

    PR-16: Reuse component

---
+++ src/components/inc/User.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "User",
+    props: {
+        users: Array
+    }
+}
+</script>
+
+<template>
+    <h2 v-for="item in users">{{ item.name }} - {{ item.email }}</h2>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,28 @@
 <script>
-import Child from "../components/inc/Child.vue";
+import User from "../components/inc/User.vue";
 export default {
   name: "Home",
   components: {
-    Child
+    User
   },
   data() {
     return {
-      user: { name: "Peter", email: "peter@gmail.com" }
-    }
-  },
-  methods: {
-    getData() {
-      console.log("Parent function called")
+      users: [
+        { name: "Peter", email: "peter@gmail.com" },
+        { name: "John", email: "john@gmail.com" },
+        { name: "Bruce", email: "bruce@gmail.com" },
+        { name: "Harry", email: "harry@gmail.com" },
+      ]
     }
   }
+
 
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
     <h1 class="text-3xl font-bold">Home Page</h1>
-    <h1 class="text-2xl font-bold">Pass data to child</h1>
-    <Child name="John wick" :user="user" :getData="getData" />
+    <User :users="users" />
 
   </main>
 </template>
