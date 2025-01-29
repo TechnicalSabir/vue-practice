7f2ef34c0d4a676e914e60d926792a8ddd59db65
TechnicalSabir <developer.sabir@gmail.com>
2025-01-29 21:46:52 +0530

    PR-21: Ref with interview question

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <script>
-import User from '@/components/inc/User.vue';
 export default {
   name: "Home",
-
-  components: {
-    User,
-  },
   methods: {
-    getUserName(name) {
-      alert(name)
+    getData() {
+      const elem = this.$refs.input;
+      elem.focus();
+      console.log(elem.value);
+      elem.style.color = "red";
     }
   }
 
@@ -17,6 +15,15 @@ export default {
 <template>
   <main class="flex justify-center items-center flex-col">
     <h1 class="text-3xl font-bold">Home Page</h1>
-    <User :getUserName="getUserName"></User>
+    <h1>Ref Example</h1>
+
+    <div class="my-4">
+      <div class="form-control">
+        <label for="">Name</label>
+        <input type="text" class="border border-gray-400 py-1 px-2 rounded-sm block" placeholder="Enter Name"
+          ref="input">
+      </div>
+      <button @click="getData" class="bg-blue-400 p-1 rounded-sm mt-5">Click me</button>
+    </div>
   </main>
 </template>
