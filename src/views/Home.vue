1b8a876ade80ba21aec19c509a3fcc939eeb3b20
SabirAdventus <mohdsabir@adventus.io>
2025-01-29 14:33:10 +0530

    PR-18: Class binding - Dynamic style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,34 @@ export default {
   name: "Home",
   data() {
     return {
-      text: "Hello world",
-      tag: `<span>Hello world</span>`
+      colorFull: true,
     }
   },
+  computed: {
+    applyStyles() {
+      return { green: this.colorFull, err: true }
+    }
+  }
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
-    <h1 class="text-3xl font-bold">Home Page</h1>
-    <div v-html="tag"></div>
+    <h1 class="text-3xl font-bold" :class="applyStyles">Home Page</h1>
+    <button class="bg-red-400 p-1 rounded-sm cursor-pointer" @click="colorFull = !colorFull">Apply Style</button>
+
 
   </main>
 </template>
+<style scoped>
+.green {
+  background-color: green;
+  color: white;
+  padding: 10px;
+  width: 200pxa;
+}
+
+.err {
+  color: red;
+  background-color: pink;
+}
+</style>
