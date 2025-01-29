2aad9d6e341dca7492fe38e168ed095b907e2653
TechnicalSabir <developer.sabir@gmail.com>
2025-01-29 21:57:06 +0530

    PR-22: Simple Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,41 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      formData: {
+        email: "",
+        password: ""
+      }
+    }
+  },
   methods: {
-    getData() {
-      const elem = this.$refs.input;
-      elem.focus();
-      console.log(elem.value);
-      elem.style.color = "red";
+    handleLogin() {
+      console.warn("login data", this.formData);
     }
   }
-
 }
 </script>
 <template>
   <main class="flex justify-center items-center flex-col">
     <h1 class="text-3xl font-bold">Home Page</h1>
-    <h1>Ref Example</h1>
+    <h1>Simple Form Example</h1>
+    <p>{{ formData }}</p>
 
     <div class="my-4">
-      <div class="form-control">
-        <label for="">Name</label>
-        <input type="text" class="border border-gray-400 py-1 px-2 rounded-sm block" placeholder="Enter Name"
-          ref="input">
-      </div>
-      <button @click="getData" class="bg-blue-400 p-1 rounded-sm mt-5">Click me</button>
+      <form action="">
+        <div class="form-control my-3">
+          <label for="">Email</label>
+          <input type="email" class="border border-gray-400 py-1 px-2 rounded-sm block" placeholder="Enter Email"
+            v-model="formData.email">
+        </div>
+        <div class="form-control my-3">
+          <label for="">Password</label>
+          <input type="text" class="border border-gray-400 py-1 px-2 rounded-sm block" placeholder="Enter Password"
+            v-model="formData.password">
+        </div>
+        <button type="button" @click="handleLogin" class="bg-blue-500 py-1 px-2 rounded-sm my-3">Login</button>
+      </form>
     </div>
   </main>
 </template>
